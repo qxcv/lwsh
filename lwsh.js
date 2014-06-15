47384d8623ebdea94d6a7a24ad11298bf5551d18
Sam Toyer <sam@qxcv.net>
2014-06-15 12:57:30 +1000

    add hacky expand/collapse

--- lwsh.js
+++ lwsh.js
@@ -1,4 +1,22 @@
 if (Meteor.isClient) {
+    Template.cameras.hidden = false;
+    Template.cameras.events({
+        'click #expandcollapse': function(templ) {
+            if (Template.cameras.hidden) {
+                /* Expand div */
+                $('#cameras').slideDown();
+                $('#expandcollapse').addClass('fa-chevron-up');
+                $('#expandcollapse').removeClass('fa-chevron-down');
+                Template.cameras.hidden = false;
+            } else {
+                /* Collapse div */
+                $('#cameras').slideUp();
+                $('#expandcollapse').addClass('fa-chevron-down');
+                $('#expandcollapse').removeClass('fa-chevron-up');
+                Template.cameras.hidden = true;
+            }
+        }
+    });
 }
 
 if (Meteor.isServer) {
